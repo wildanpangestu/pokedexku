a2be576568266631bdcb9018cd7575eb1adcbb1a
Wildan Pangestu <wildan.pangestu@btpnsyariah.com>
2021-01-17 20:36:03 +0700

    Updates

---
+++ precache-manifest.6e9ac88344cc9b4fd4c124a83876764d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293f58b89ee416b19730a08cfa156a95",
+    "url": "/pokedexku/index.html"
+  },
+  {
+    "revision": "e49bf3e2757b97989afb",
+    "url": "/pokedexku/static/css/main.29e49d84.chunk.css"
+  },
+  {
+    "revision": "2ac422633710c0747e30",
+    "url": "/pokedexku/static/js/2.d4912209.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/pokedexku/static/js/2.d4912209.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e49bf3e2757b97989afb",
+    "url": "/pokedexku/static/js/main.04913c75.chunk.js"
+  },
+  {
+    "revision": "9c739f289ecee06b7f62",
+    "url": "/pokedexku/static/js/runtime-main.8ee932cd.js"
+  }
+]);
